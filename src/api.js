bda2199b4bdcb818ccf261e5f8fffc5d5407d6d2
yevheniidolhov-boop <yevhenii.dolhov@skelar.tech>
2025-11-07 12:19:29 +0200

    Initial upload
    Initial upload

---
+++ src/api.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://script.google.com/a/macros/skelar.tech/s/AKfycbzB9Vy5iftkxFBn6_giZSsyZUJf-oZzhDIZ29yzKNBRJfPPvEdKZumjJUoYtz8ECeipNA/exec"; 
+export const API_TOKEN = "HabitONE";
